7b592fe3983d22c428c6f4e5b41d4c924ffd4026
Adam Saegebarth <adam.saegebarth@gmail.com>
2025-02-23 17:24:00 -0800

    add analog rhythms post

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -41,6 +41,9 @@ const config: GatsbyConfig = {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.mdx`, `.md`], // Ensure Gatsby processes both .mdx and .md files
+        mdxOptions: {
+          useDynamicImport: true,
+        },
       },
     },
     `gatsby-plugin-react-helmet`,
